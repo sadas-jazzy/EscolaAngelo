33953e7e279a4d582b67f55d13af199e137521c2
sadas-jazzy <104647116+sadas-jazzy@users.noreply.github.com>
2024-04-24 08:10:49 -0300

    Add files via upload

---
+++ dssonauro.py
@@ -0,0 +1,14 @@
+listaNomes = []
+
+
+listaNomes.append("edson")
+listaNomes.append("Tamires")
+listaNomes.append("Sofia")
+
+print(listaNomes)
+print(listaNomes[1])
+
+for nome in listaNomes:
+    print(nome)
+
+primeiroDicionario['nome'] = "Edson"
